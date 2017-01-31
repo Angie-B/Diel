6039ad6e9498229ae3c56fa3aedd2762fc6141f2
Angie Boysen <akboysen@gmail.com>
2017-01-31 15:26:53 -0800

    added a function at the end that can put multiple ggplots on one fig

--- Line.Plots.All.Diel.R
+++ Line.Plots.All.Diel.R
@@ -145,4 +145,35 @@ p3 <- ggplot(plot.data, aes(x=HourOfExperiment, y=mean,
                             group = Compound.Name)) +
      geom_line(aes(color = as.factor(S))) + facet_wrap(~S)
 
+## multiplot ---
+multiplot <- function(..., plotlist = NULL, file, cols = 1, layout = NULL) {
+     require(grid)
+     
+     plots <- c(list(...), plotlist)
+     
+     numPlots = length(plots)
+     
+     if (is.null(layout)) {
+          layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                           ncol = cols, nrow = ceiling(numPlots/cols))
+     }
+     
+     if (numPlots == 1) {
+          print(plots[[1]])
+          
+     } else {
+          grid.newpage()
+          pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+          
+          for (i in 1:numPlots) {
+               matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+               
+               print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                               layout.pos.col = matchidx$col))
+          }
+     }
+}
+multiplot(p1,p2, cols=1)
 
+
+par(mfrow=c(1,1))
